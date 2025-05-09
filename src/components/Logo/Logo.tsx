6546e55c97bef6c8ae7bade8f79626969179ffd2
Marcus <marcusdeleon1983@gmail.com>
2025-05-08 22:11:58 -0500

    update logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -9,34 +9,36 @@ interface Props {
 
 export const Logo = ({ loading = 'lazy', priority = 'low', className }: Props) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 72 72"
-      className={clsx(className)}
-      aria-hidden="true"
-    >
-      <circle className="logo-fill" cx="36" cy="36" r="36" />
-      <polyline className="logo-stroke" points="22.63 57 17.25 52.51 12 57" />
-      <polyline className="logo-stroke" points="12 15 17.25 19.5 24.99 19.5 29.13 15" />
-      <line className="logo-stroke" x1="36" y1="48.59" x2="31.35" y2="53.08" />
-      <line className="logo-stroke" x1="40.63" y1="53.08" x2="36" y2="48.59" />
-      <line className="logo-stroke" x1="17.25" y1="52.51" x2="17.25" y2="19.5" />
-      <line className="logo-stroke" x1="24.99" y1="19.5" x2="36" y2="48.59" />
-      <polyline className="logo-stroke" points="49.37 57 54.75 52.51 60 57" />
-      <polyline className="logo-stroke" points="60 15 54.75 19.5 47.01 19.5 42.87 15" />
-      <line className="logo-stroke" x1="54.75" y1="52.51" x2="54.75" y2="19.5" />
-      <line className="logo-stroke" x1="47.01" y1="19.5" x2="36" y2="48.59" />
-      <polygon
-        className="logo-stroke"
-        points="40.63 53.08 31.35 53.08 22.63 25.87 22.63 57 12 57 12 15 29.17 15 36 34.71 42.83 15 60 15 60 57 49.37 57 49.37 25.87 40.63 53.08"
-      />
-      <polyline className="logo-stroke" points="17.25 19.5 22.63 25.87 24.99 19.5" />
-      <polyline className="logo-stroke" points="54.75 19.5 49.37 25.87 47.01 19.5" />
-      <line className="logo-stroke" x1="36" y1="34.71" x2="36" y2="48.59" />
-      <style>{`
-        .logo-fill { fill: #293443; }
-        .logo-stroke { fill: none; stroke: #fff; stroke-width: 2px; }
-      `}</style>
+    <svg width="72" height="72" viewBox="0 0 72 72" fill="none">
+      <g clip-path="url(#clip0_118_113)">
+        <path
+          d="M36 72C55.8823 72 72 55.8823 72 36C72 16.1177 55.8823 0 36 0C16.1177 0 0 16.1177 0 36C0 55.8823 16.1177 72 36 72Z"
+          fill="black"
+        />
+        <path d="M22.63 57L17.25 52.51L12 57" stroke="white" stroke-width="2" />
+        <path d="M12 15L17.25 19.5H24.99L29.13 15" stroke="white" stroke-width="2" />
+        <path d="M35.9996 48.5901L31.3496 53.0801" stroke="white" stroke-width="2" />
+        <path d="M40.63 53.0801L36 48.5901" stroke="white" stroke-width="2" />
+        <path d="M17.25 52.51V19.5" stroke="white" stroke-width="2" />
+        <path d="M24.9902 19.5L36.0002 48.59" stroke="white" stroke-width="2" />
+        <path d="M49.3701 57L54.7501 52.51L60.0001 57" stroke="white" stroke-width="2" />
+        <path d="M60.0001 15L54.7501 19.5H47.0101L42.8701 15" stroke="white" stroke-width="2" />
+        <path d="M54.75 52.51V19.5" stroke="white" stroke-width="2" />
+        <path d="M47.01 19.5L36 48.59" stroke="white" stroke-width="2" />
+        <path
+          d="M40.63 53.08H31.35L22.63 25.87V57H12V15H29.17L36 34.71L42.83 15H60V57H49.37V25.87L40.63 53.08Z"
+          stroke="white"
+          stroke-width="2"
+        />
+        <path d="M17.25 19.5L22.63 25.87L24.99 19.5" stroke="white" stroke-width="2" />
+        <path d="M54.7498 19.5L49.3698 25.87L47.0098 19.5" stroke="white" stroke-width="2" />
+        <path d="M36 34.71V48.59" stroke="white" stroke-width="2" />
+      </g>
+      <defs>
+        <clipPath id="clip0_118_113">
+          <rect width="72" height="72" fill="white" />
+        </clipPath>
+      </defs>
     </svg>
   )
 }
